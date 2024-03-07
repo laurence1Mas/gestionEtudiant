61c4c32e8b923816539af4ec76dd1fb27fb64a15
laurence1Mas <119684303+laurence1Mas@users.noreply.github.com>
2024-03-07 15:05:50 +0100

    second commit

--- AbstractInterfaceJavaCSharp-main/Java/src/com/josamuna/project/interfaceusage/IPerson.java
+++
@@ -1,9 +0,0 @@
-package com.josamuna.project.interfaceusage;
-
-import java.sql.SQLException;
-
-public interface IPerson {
-	void showIdentity();
-	void showDynamicIdentity(int id) throws SQLException;
-	int add() throws SQLException;
-}
--- AbstractInterfaceJavaCSharp-main/Java/src/com/josamuna/projects/connectionsample/IConnection.java
+++
@@ -1,8 +0,0 @@
-package com.josamuna.projects.connectionsample;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface IConnection {
-	Connection getConnection() throws SQLException;
-}
